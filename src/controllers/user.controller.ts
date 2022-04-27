668c9e5f1b0e644f6ef7d566c91d19fce3aa7b88
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-04-28 03:52:01 +0530

    User Get and Post Endpoints and Controllers added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,7 +11,12 @@ const prisma = new PrismaClient()
  * @param res Response Object
  * @description Get Users
  */
-const getUsers = async (req: Request, res: Response) => {
+const getUsers = async (
+  req: Request<{}, {}, {}, { country: string; city: string }>,
+  res: Response
+) => {
+  console.log(req.query.country)
+
   const owners = await prisma.owners.findMany({
     where: {
       email: {
@@ -22,6 +27,55 @@ const getUsers = async (req: Request, res: Response) => {
   res.json({ users: owners })
 }
 
+/**
+ * @route GET /api/user/:email
+ * @access Public
+ * @param req Request Object
+ * @param res Response Object
+ * @description Get Users
+ */
+const getUser = async (
+  req: Request<{ email: "string" }, {}, {}, {}>,
+  res: Response
+) => {
+  const owners = await prisma.owners.findFirst({
+    where: {
+      email: {
+        contains: req.params.email,
+      },
+    },
+  })
+  res.json({ users: owners })
+}
+
+/**
+ * @route POST /api/user/:email
+ * @access Public
+ * @param req Request Object
+ * @param res Response Object
+ * @description Get Users
+ */
+const createUser = async (
+  req: Request<
+    {},
+    {},
+    {
+      first_name: string
+      last_name: string
+      full_name: string
+      email: string
+    },
+    {}
+  >,
+  res: Response
+) => {
+  console.log(req.body.email)
+
+  res.json({ message: "User Created" })
+}
+
 export default {
   getUsers,
+  getUser,
+  createUser,
 }
--- src/router/user.ts
+++ src/router/user.ts
@@ -5,5 +5,7 @@ import userController from "../controllers/user.controller"
 const userRouter = Router()
 
 userRouter.get("/", userController.getUsers)
+userRouter.get("/:email", userController.getUser)
+userRouter.post("/", userController.createUser)
 
 export default userRouter
